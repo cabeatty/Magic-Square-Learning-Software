71407511cac9ab50c94de175ef15fa9c1951263a
Colin Beatty <cabeatty@oakland.edu>
2017-02-21 18:26:37 -0500

    Migrated methods to a single header, (solver.h)

--- Headers/doublyEven.h
+++
@@ -1,9 +0,0 @@
-/**
- * Created by colin on 15/02/17.
- * This class will contain methods for solving d-even magic squares
-**/
-
-#ifndef MIDTERM_ALT_DOUBLYEVEN_H
-#define MIDTERM_ALT_DOUBLYEVEN_H
-
-#endif //MIDTERM_ALT_DOUBLYEVEN_H
---
+++ Headers/solver.h
@@ -0,0 +1,79 @@
+/**
+ * Created by colin on 15/02/17.
+ * This class will contain methods for solving s-even magic squares
+**/
+#define MIDTERM_ALT_SINGLYEVEN_H
+
+using namespace std;
+
+void hello()
+{
+	cout << "hello" << endl;
+}
+
+void odd(vector<vector<int>> &square, int n)
+{
+	//TODO: visualization.  print the square at each step
+	int i = n / 2;
+	int j = n - 1;
+
+	for (int num = 1; num <= n * n;) {
+		if (i == -1 && j == n) //3rd condition
+		{
+			j = n - 2;
+			i = 0;
+		}
+		else
+		{
+			if (j == n) //1st condition helper if next number goes to out of square's right side
+				j = 0;
+			if (i < 0)  //1st condition helper if next number is goes to out of square's upper side
+				i = n - 1;
+		}
+		if (square[i][j]) //2nd condition
+		{
+			j -= 2;
+			i++;
+			continue;
+		} else
+			square[i][j] = num++; //set number
+		//TODO print should probably be here, where we actually make changes
+
+		j++;
+		i--; //1st condition
+
+	}
+}
+
+void dEven(vector<vector<int>> &square, int n)
+{
+	int i, j;
+	vector<vector<int>> I(n, vector<int>(n, 0));
+	vector<vector<int>> J(n, vector<int>(n, 0));
+	int index = 1;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			I[i][j] = ((i + 1) % 4) / 2;
+			J[j][i] = ((i + 1) % 4) / 2;
+			square[i][j] = index;
+			index++;
+		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (I[i][j] == J[i][j])
+			{
+				square[i][j] = n * n + 1 - square[i][j];
+			}
+		}
+	}
+}
+
+void sEven(vector<vector<int>> &square, int n)
+{
+
+}
